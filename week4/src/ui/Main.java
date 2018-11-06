045f8ab35fffb8814ae01611478b2e1a135df815
treykris <trey10027@gmail.com>
2018-11-06 17:31:57 -0500

    Added main examples

--- week4/src/ui/Main.java
+++ week4/src/ui/Main.java
@@ -1,18 +1,37 @@
 package ui;
 
 
-import models.Date;
+import models.*;
 
 public class Main {
     public static void main(String[] args) {
-        Date dateCheck = new Date(11, 7, 1993);
+        Calendar cal1 = new Calendar(new Date(4, 8, 2018));
+        cal1.setEmail("123@gmail.com");
+
+        Date tomorrow = new Date(11, 7, 2018);
+        Date nextWeek = new Date(11, 13, 2018);
+        Date nextMonth = new Date(12, 6, 2018);
+
+        Reminder r1 = new Reminder(tomorrow, new Time(6, 0), "Study classes");
+        r1.setNote("Make sure to complete final project before 9pm");
 
 
-        dateCheck.getDay();
-        dateCheck.getMonth();
-        dateCheck.getYear();
-        dateCheck.getDateShortFormat();
-        dateCheck.getDateLongFormat();
+        cal1.addEntry(r1);
+        cal1.addEntry(new Event(nextWeek, new Time(12, 00), "Lunch with Chloe"));
+        Meeting m = new Meeting(nextMonth, new Time(9, 0), "Testing 101");
+
+        m.addAttendee("joey123@gmail.com");
+        m.addAttendee("chloe123@gmail.com");
+
+        cal1.addEntry(m);
+
+        m.sendInvites();
+        System.out.println();
+
+        cal1.printEntries();
+
+
+
 
 
 
