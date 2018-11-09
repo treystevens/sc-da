67b3a450e0af70214a775523e36b4729ccd990fa
treykris <trey10027@gmail.com>
2018-11-08 22:12:33 -0500

    Implemented Zookeeper class

--- week4/TPD-practice-projects/ZooManager/src/model/Zookeeper.java
+++ week4/TPD-practice-projects/ZooManager/src/model/Zookeeper.java
@@ -1,6 +1,8 @@
 package model;
 
 
+import java.util.List;
+
 public class Zookeeper {
 
     private String name;
@@ -8,7 +10,40 @@ public class Zookeeper {
     private List<Animal> animalList;
     private Animal favourite;
 
-    // TODO: follow the instructions on the webpage to implement this class
+    public Zookeeper(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
 
+
+    // GETTERS
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public List<Animal> getAnimals(){
+        return animalList;
+    }
+    public Animal getFavourite(){
+        return favourite;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Sets the zookeeper's favorite animal
+    public void setFavourite(Animal favourite){
+        this.favourite = favourite;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds an animal to the list of animals the zookeeper takes care of
+    public void addAnimal(Animal a){
+        animalList.add(a);
+    }
+
+    public void removeAnimal(Animal a){
+        animalList.remove(a);
+    }
 
 }
